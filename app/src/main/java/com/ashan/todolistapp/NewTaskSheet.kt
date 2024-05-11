f1a75edc877162cd14be121e23d6a821379730f2
ashanthilochana <ashanthilochana98@gmail.com>
2024-05-11 22:50:48 +0530

    Create Task Sheet View And Bindings

---
+++ app/src/main/java/com/ashan/todolistapp/NewTaskSheet.kt
@@ -0,0 +1,21 @@
+package com.ashan.todolistapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+
+class NewTaskSheet : BottomSheetDialogFragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_new_task_sheet, container, false)
+    }
+
+}
---
+++ app/src/main/java/com/ashan/todolistapp/TaskViewModel.kt
@@ -0,0 +1,11 @@
+package com.ashan.todolistapp
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class TaskViewModel: ViewModel() {
+
+    var name = MutableLiveData<String>()
+    var desc = MutableLiveData<String>()
+
+}
