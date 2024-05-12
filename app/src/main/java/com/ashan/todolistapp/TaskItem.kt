fdd425fee98b9632332a88feadda44e67c4d26da
ashanthilochana <ashanthilochana98@gmail.com>
2024-05-12 20:29:41 +0530

    Create update task functions

---
+++ app/src/main/java/com/ashan/todolistapp/TaskItem.kt
@@ -0,0 +1,16 @@
+package com.ashan.todolistapp
+
+import java.time.LocalDate
+import java.time.LocalTime
+import java.util.UUID
+
+class TaskItem(
+    var name: String,
+    var desc: String,
+    var dueTime: LocalTime?,
+    var completedDate: LocalDate?,
+    var id: UUID = UUID.randomUUID()
+)
+{
+
+}
